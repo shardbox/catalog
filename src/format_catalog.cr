3301e3366c8d0d3234b60dea24c97bee90f972b7
Johannes Müller <straightshoota@gmail.com>
2019-11-08 17:34:34 +0100

    Add warning for duplicate mirrors

--- src/format_catalog.cr
+++ src/format_catalog.cr
@@ -3,11 +3,21 @@ require "shardbox-core/catalog"
 
 catalog_path = "catalog"
 
+entries = {} of Repo::Ref => Catalog::Entry
+mirrors = Set(Repo::Ref).new
+
 Catalog.each_category(catalog_path) do |yaml_category, category_slug|
   shards = yaml_category.shards
   if category_slug == "Uncategorized"
     if !shards.empty?
       warn "Category 'Uncategorized' must not contain any entries.", category_slug
+    end
+  end
+
+  yaml_category.shards.each do |shard|
+    entries[shard.repo_ref] ||= shard
+    if duplicate_repo = Catalog.duplicate_mirror?(shard, mirrors, entries)
+      warn "duplicate mirror #{duplicate_repo}", category_slug
     end
   end
 
@@ -25,7 +35,7 @@ Catalog.each_category(catalog_path) do |yaml_category, category_slug|
     a, b = cons
     if a.repo_ref == b.repo_ref
       # Try to remove duplicate entries automatically
-      if a.mirror == b.mirror && a.legacy == b.legacy
+      if a.mirrors == b.mirrors
         if a.description == b.description || a.description.nil?
           shards.delete(a)
           next
