9351dfebd06fdb727357ac8a6694247acf9d5051
Johannes Müller <straightshoota@gmail.com>
2020-01-31 17:41:55 +0100

    Improve duplicates recognition

--- src/formatter.cr
+++ src/formatter.cr
@@ -3,18 +3,16 @@ require "./tools"
 
 module Catalog::Tools
   def self.command_format
-    all_entries = {} of Repo::Ref => Catalog::Entry
-    all_mirrors = Set(Repo::Ref).new
     has_warnings = false
+    duplication = Catalog::Duplication.new
 
     catalog = Catalog.new(catalog_path)
     catalog.each_category do |category|
       warnings = Catalog::Tools.normalize_category(category)
 
       category.shards.each do |shard|
-        all_entries[shard.repo_ref] ||= shard
-        if duplicate_repo = Catalog.duplicate_mirror?(shard, all_mirrors, all_entries)
-          warnings << "Duplicate mirror #{duplicate_repo}."
+        if error = duplication.register(category.slug, shard)
+          warnings << error.message
         end
       end
 
